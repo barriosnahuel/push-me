97894c0d009846d112bdf8ee4f34fd08ad6490eb
Nahuel Barrios <barrios.nahuel@gmail.com>
2019-07-13 21:40:48 -0300

    Better usage of Kotlin

--- model/src/main/java/com/github/barriosnahuel/vossosunboton/model/Sound.kt
+++ model/src/main/java/com/github/barriosnahuel/vossosunboton/model/Sound.kt
@@ -2,7 +2,7 @@ package com.github.barriosnahuel.vossosunboton.model
 
 import androidx.annotation.RawRes
 
-data class Sound(val name: String?, val file: String?, @RawRes val rawRes: Int?) {
-    constructor(name: String?, file: String?) : this(name, file, 0)
-    constructor(name: String?, @RawRes rawRes: Int?) : this(name, null, rawRes)
+data class Sound(val name: String, val file: String?, @RawRes val rawRes: Int) {
+    constructor(name: String, file: String?) : this(name, file, 0)
+    constructor(name: String, @RawRes rawRes: Int = 0) : this(name, null, rawRes)
 }

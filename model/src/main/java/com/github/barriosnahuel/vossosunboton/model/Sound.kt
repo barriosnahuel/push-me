c5213d21ade9d302f8fed17622c903487aa4f271
Nahuel Barrios <barrios.nahuel@gmail.com>
2019-07-18 07:57:54 -0300

    Add docs to Sound.kt

--- model/src/main/java/com/github/barriosnahuel/vossosunboton/model/Sound.kt
+++ model/src/main/java/com/github/barriosnahuel/vossosunboton/model/Sound.kt
@@ -2,6 +2,9 @@ package com.github.barriosnahuel.vossosunboton.model
 
 import androidx.annotation.RawRes
 
+/**
+ * Represents a sound (a.k.a. a button of this app).
+ */
 data class Sound(val name: String, val file: String?, @RawRes val rawRes: Int) {
     constructor(name: String, file: String?) : this(name, file, 0)
     constructor(name: String, @RawRes rawRes: Int = 0) : this(name, null, rawRes)

2a2bd546912457423feaaebfd6b3ffd00a0bc3c0
Nahuel Barrios <barrios.nahuel@gmail.com>
2019-10-20 14:04:11 -0300

    Apply suggestions from code checkers

--- model/src/main/java/com/github/barriosnahuel/vossosunboton/model/Sound.kt
+++ model/src/main/java/com/github/barriosnahuel/vossosunboton/model/Sound.kt
@@ -9,5 +9,8 @@ data class Sound(val name: String, val file: String?, @RawRes val rawRes: Int) {
     constructor(name: String, file: String?) : this(name, file, 0)
     constructor(name: String, @RawRes rawRes: Int = 0) : this(name, null, rawRes)
 
+    /**
+     * Indicates whether or not this sound is bundled in the app or is a user's custom sound.
+     */
     fun isBundled() = file == null
 }

2c5e9f96b02b589d921d4f8d64e517db7d70262f
Nahuel Barrios <barrios.nahuel@gmail.com>
2019-11-09 19:23:41 -0300

    App: Add tests for the share feature

---
+++ app/src/test/java/com/github/barriosnahuel/vossosunboton/TestApplication.kt
@@ -0,0 +1,17 @@
+package com.github.barriosnahuel.vossosunboton
+
+import android.app.Application
+import org.robolectric.shadows.ShadowLog
+import timber.log.Timber
+
+class TestApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        ShadowLog.stream = System.out
+
+        Timber.plant(Timber.DebugTree())
+        Timber.d("Creating TEST (%s) application...", BuildConfig.BUILD_TYPE)
+    }
+}
--- app/src/test/java/com/github/barriosnahuel/vossosunboton/feature/share/ShareFeatureImplTest.kt
+++ app/src/test/java/com/github/barriosnahuel/vossosunboton/feature/share/ShareFeatureImplTest.kt
@@ -1,31 +1,82 @@
 package com.github.barriosnahuel.vossosunboton.feature.share
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import androidx.core.content.FileProvider
+import androidx.test.core.app.ApplicationProvider
+import com.github.barriosnahuel.vossosunboton.AbstractRobolectricTest
 import com.github.barriosnahuel.vossosunboton.model.Sound
-import com.google.common.truth.Truth
+import com.google.common.truth.Truth.assertThat
+import io.mockk.every
+import io.mockk.mockkStatic
+import io.mockk.slot
+import io.mockk.spyk
 import org.junit.Test
-import org.mockito.Mockito.mock
 
-internal class ShareFeatureImplTest {
+internal class ShareFeatureImplTest : AbstractRobolectricTest() {
 
     @Test
-    fun `share when sound URI is null must throw an exception`() {
+    fun `share when sound Uri is null must throw an exception`() {
         val sound = givenASoundWithNullUri()
 
         try {
-            whenTryingToShareIt(sound)
+            whenSharingThe(sound)
         } catch (e: IllegalStateException) {
             thenWeThrowAnIllegalStateException(e)
         }
     }
 
-    private fun givenASoundWithNullUri() = Sound("name")
+    @Test
+    fun `share when sound URI is valid show disambiguation window`() {
+        val sound = givenASound()
 
-    private fun whenTryingToShareIt(sound: Sound) {
-        ShareFeature.instance.share(mock(Context::class.java), sound)
+        val generatedIntent = whenSharingThe(sound)
+
+        thenOSShowDisambiguationWindow(generatedIntent)
+    }
+
+    @Test
+    fun `share when sound URI is valid sends the audio file`() {
+        val sound = givenASound()
+
+        val generatedIntent = whenSharingThe(sound)
+
+        thenWeSendAnAudio(generatedIntent)
+    }
+
+    private fun givenASound() = Sound("my button name", "a/dummy/sound/uri")
+
+    private fun givenASoundWithNullUri() = Sound("my button name")
+
+    private fun whenSharingThe(sound: Sound): Intent {
+        val mockedContext = spyk<Context>(ApplicationProvider.getApplicationContext<Context>())
+
+        mockkStatic(FileProvider::class)
+        every { FileProvider.getUriForFile(mockedContext, any(), any()) } returns Uri.EMPTY
+
+        val slot = slot<Intent>()
+        every { mockedContext.startActivity(capture(slot)) } answers { nothing }
+
+        ShareFeature.instance.share(mockedContext, sound)
+
+        return slot.captured
     }
 
     private fun thenWeThrowAnIllegalStateException(e: IllegalStateException) {
-        Truth.assertThat(e.message).isEqualTo("File URI is mandatory")
+        assertThat(e.message).isEqualTo("File URI is mandatory")
+    }
+
+    private fun thenOSShowDisambiguationWindow(intent: Intent) {
+        assertThat(intent.action).isEqualTo(Intent.ACTION_CHOOSER)
+        assertThat(intent.extras?.containsKey(Intent.EXTRA_INTENT)).isTrue()
+    }
+
+    private fun thenWeSendAnAudio(intent: Intent) {
+        val shareIntent = intent.getParcelableExtra<Intent>(Intent.EXTRA_INTENT)
+
+        assertThat(shareIntent?.action).isEqualTo(Intent.ACTION_SEND)
+        assertThat(shareIntent?.type).isEqualTo("audio/*")
+        assertThat(shareIntent?.extras?.containsKey(Intent.EXTRA_STREAM)).isTrue()
     }
 }

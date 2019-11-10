7c7107a4f4a35618dd0d8a2f68b8355ac6bfb40d
Nahuel Barrios <barrios.nahuel@gmail.com>
2019-11-10 00:26:23 -0300

    Better code formatting

--- app/src/main/java/com/github/barriosnahuel/vossosunboton/ui/home/UndoSwipeDismissListener.kt
+++ app/src/main/java/com/github/barriosnahuel/vossosunboton/ui/home/UndoSwipeDismissListener.kt
@@ -3,9 +3,14 @@ package com.github.barriosnahuel.vossosunboton.ui.home
 import android.view.View
 import com.github.barriosnahuel.vossosunboton.model.Sound
 
-internal class UndoSwipeDismissListener(private val soundsAdapter: SoundsAdapter, private val soundToRemove: Sound, private val position: Int) : View
-.OnClickListener {
+internal class UndoSwipeDismissListener(
+        private val soundsAdapter: SoundsAdapter,
+        private val soundToRemove: Sound,
+        private val position: Int
+) : View.OnClickListener {
+
     override fun onClick(v: View?) {
         soundsAdapter.restore(soundToRemove, position)
     }
+
 }

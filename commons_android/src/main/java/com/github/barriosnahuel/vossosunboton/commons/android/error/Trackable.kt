772b8f0e6da66abf2664015cb7baaef6d359b1ee
Nahuel Barrios <barrios.nahuel@gmail.com>
2020-02-29 17:09:00 -0300

    Change ErrorTrackerTree's visibility to internal for its module

--- commons_android/src/main/java/com/github/barriosnahuel/vossosunboton/commons/android/error/Trackable.kt
+++ commons_android/src/main/java/com/github/barriosnahuel/vossosunboton/commons/android/error/Trackable.kt
@@ -38,7 +38,7 @@ object Tracker : Trackable {
     }
 }
 
-class ErrorTrackerTree : Timber.Tree() {
+internal class ErrorTrackerTree : Timber.Tree() {
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
         Tracker.log(message)
     }

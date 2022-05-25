d0027c1e1577e355657b9da4d542dc8c9483c861
Nahuel Barrios <barrios.nahuel@gmail.com>
2022-05-25 12:34:31 -0300

    Apply minor detekt suggestions

--- app/src/test/java/com/github/barriosnahuel/vossosunboton/AbstractRobolectricTest.kt
+++ app/src/test/java/com/github/barriosnahuel/vossosunboton/AbstractRobolectricTest.kt
@@ -5,6 +5,10 @@ import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 
+/**
+ * Suppress detekt suggestion because annotations RunWith and Config are required to all tests.
+ */
+@Suppress("UnnecessaryAbstractClass")
 @RunWith(RobolectricTestRunner::class)
 @Config(sdk = [Build.VERSION_CODES.LOLLIPOP, Build.VERSION_CODES.S_V2], application = TestApplication::class)
 internal abstract class AbstractRobolectricTest

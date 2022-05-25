0d7c062d08df89ce078a387937cdfd89d7bef8e7
Nahuel Barrios <barrios.nahuel@gmail.com>
2022-05-25 11:13:45 -0300

    Make tests run for API 32 instead of 28

--- app/src/test/java/com/github/barriosnahuel/vossosunboton/AbstractRobolectricTest.kt
+++ app/src/test/java/com/github/barriosnahuel/vossosunboton/AbstractRobolectricTest.kt
@@ -6,5 +6,5 @@ import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
-@Config(sdk = [Build.VERSION_CODES.LOLLIPOP, Build.VERSION_CODES.P], application = TestApplication::class)
+@Config(sdk = [Build.VERSION_CODES.LOLLIPOP, Build.VERSION_CODES.S_V2], application = TestApplication::class)
 internal abstract class AbstractRobolectricTest
